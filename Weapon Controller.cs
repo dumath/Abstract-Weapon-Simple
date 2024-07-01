377939a59b81fa8948f5dcaaea4d195ce05b4d5f
dumath <34304654+dumath@users.noreply.github.com>
2024-07-01 13:40:39 +0300

    Update Weapon Controller.cs

--- Weapon Controller.cs
+++ Weapon Controller.cs
@@ -4,78 +4,53 @@ using UnityEngine;
 using UnityEngine.Pool;
 using System;
 
-public sealed class WeaponController : MonoBehaviour
+public abstract class WeaponController : MonoBehaviour
 {
     #region Constants
     public const float ACCURACY = 0.9998f;
     #endregion
 
-    #region Object Properties
-    [SerializeField] private Weapon weapon; // Турель.
-    [SerializeField] private Fastener fastener; // Крепеж турели.
-
-    [SerializeField] private GameObject barrel; // Дуло.
+    #region Object propertyies
+    [SerializeField] protected Weapon weapon; // Турель.
+    [SerializeField] protected Fastener fastener; // Крепеж турели.
+    [SerializeField] protected Transform weaponSight; // Прицел оружия.
 
     [SerializeField] private GameObject ammoPrefab; // Шаблон боеприпаса.
 
-    private Action<Transform> setTargetAction; // Установщик целей, для наводки.
+    protected Action<Transform> setTargetAction; // Установщик целей, для наводки.
 
     public float Strength = 5f; // Сила выстрела.
     public float RepeatRate = 2.0f; //Темп стрельбы.
 
-    private List<Transform> targets; // Список очереди целеуказания.
-    private ObjectPool<GameObject> pool; // Пул боеприпасов. 
+    protected float elapsedTime = 0f; // Пройденное время. При необходимости убрать задержку первого выстрела, со старта сцены.
 
-    private float elapsedTime = 0f; //Пройденное время. При необходимости убрать задержку первого выстрела, со старта сцены.
+    protected List<Transform> targets; // Список очереди целеуказания.
+
+    protected ObjectPool<Ammunition> pool; // Пул боеприпасов. 
+    public int MaxSizePool = 10;
     #endregion
 
-    #region Mono Methods
-    // Start is called before the first frame update
-    void Start()
+    #region Mono
+    protected virtual void Awake()
     {
         //Инициализируем Pool.
-        pool = new ObjectPool<GameObject> (OnCreateAmmo, OnGetAmmoFromPool, OnReleaseAmmoToPool, OnDestroyAmmoFromPool, maxSize: 10);
+        pool = new ObjectPool<Ammunition>(OnCreateAmmo, OnGetAmmoFromPool, OnReleaseAmmoToPool, OnDestroyAmmoFromPool, maxSize: MaxSizePool);
 
         //Инициализируем коллекцию.
         targets = new List<Transform>();
 
         //Крепим к делегату методы уведомлений двух поворотных частей.
-        setTargetAction = new Action<Transform>(fastener.OnTargetChange);
+        setTargetAction += fastener.OnTargetChange;
         setTargetAction += weapon.OnTargetChange;
     }
 
+    // Start is called before the first frame update
+    protected abstract void Start();
+
     // Update is called once per frame
-    void Update()
-    {
-        if(targets.Count != 0)
-        {
-            // Находим вектор направления до цели.
-            Vector3 direction = targets.First().transform.position - barrel.transform.position;
+    protected abstract void Update();
 
-            // Смотрим сонаправленность векторов цели и дула.
-            float dotCondition = Vector3.Dot(direction.normalized, barrel.transform.forward);
-
-            if(dotCondition > ACCURACY)
-            {
-                // Реализация простого счетчика пройденного времени.
-                // Можно убрать задержку 2f, со старта сцены, присвоив полю, в месте объявления 2f.
-                // Left as is.
-                elapsedTime += Time.deltaTime;
-                if (elapsedTime >= RepeatRate)
-                {
-                    // Стреляем, обновляем таймер пройденного времени.
-                    Fire();
-                    elapsedTime = 0f;
-                }
-            }
-        }
-        
-        // Debug. Оставлен, видеть наводку(угол вертикальный с ограничителем + горизонтальный),
-        // Выстрел снаряда на выходе с пула со старта и после.
-        Debug.DrawRay(barrel.transform.position, barrel.transform.forward * 20f, Color.red);
-    }
-
-    private void OnTriggerEnter(Collider other)
+    protected virtual void OnTriggerEnter(Collider other)
     {
         //  Добавляем трансформ в список, для отслеживания.
         targets.Add(other.gameObject.transform);
@@ -84,7 +59,7 @@ public sealed class WeaponController : MonoBehaviour
         setTargetAction(targets.First());
     }
 
-    private void OnTriggerExit(Collider other)
+    protected virtual void OnTriggerExit(Collider other)
     {
         // Удаляем первый вход, поскольку вышел из области видимости турели.
         targets.Remove(other.gameObject.transform);
@@ -96,67 +71,33 @@ public sealed class WeaponController : MonoBehaviour
     #endregion
 
     #region Weapon Controller methods
-    public void Fire()
-    {
-        // Берем объект из пула.
-        GameObject ammo = pool.Get();
-        
-        // Задаем начальную точку. В данном случае якорь дула (Якорь выставлен в Blender'e).
-        ammo.transform.position = barrel.transform.position;
-        ammo.transform.rotation = barrel.transform.rotation;
-        ammo.GetComponent<Rigidbody>().AddForce(barrel.transform.forward * Strength, ForceMode.Impulse);
-    }
+    // Базовая реализация используется для турельного типа
+    public abstract void Fire();
     #endregion
 
     #region Object Pool
-    /* Описание методов Intelli оставлено. На случай убрать под свой пул, либо поменять логику пула Unity. */
-
-    /// <summary>
-    /// Метод, вызываемый ObjectPool<>.Get(), если пул пустой.
-    /// </summary>
-    /// <returns> Созданный объект пула из префаба. </returns>
-    public GameObject OnCreateAmmo()
+    // Метод, вызываемый ObjectPool<>.Get(), если пул пустой.
+    public Ammunition OnCreateAmmo()
     {
         //Создаем объект из префаба и назначаем ему callback. (Один раз!).
         //Мидификатор readonly - не использую. Обычная проверка на NULL.
         GameObject instance = Instantiate(ammoPrefab);
         instance.GetComponent<Ammunition>().SetActionOnReturn(pool.Release);
 
-        return instance;
+        return instance.GetComponent<Ammunition>();
     }
 
-    /// <summary>
-    /// Метод, вызываемый ObjectPool<>.Get.
-    /// </summary>
-    /// <param name="item"> Объект, из пула. </param>
-    public void OnGetAmmoFromPool(GameObject item)
+    // Метод, вызываемый ObjectPool<>.Get.
+    public void OnGetAmmoFromPool(Ammunition item) => item.gameObject.SetActive(true);
+
+    // Метод, вызываемый ObjectPool<>.Release().
+    public void OnReleaseAmmoToPool(Ammunition item)
     {
-        // Включаем влияение физики, а затем сам объект.
-        //item.GetComponent<Rigidbody>().detectCollisions = true;
-        //item.GetComponent<Rigidbody>().useGravity = true;
-        item.SetActive(true);
+        item.ExhaustForce();
+        item.gameObject.SetActive(false);
     }
 
-    /// <summary>
-    /// Метод, вызываемый ObjectPool<>.Release().
-    /// </summary>
-    /// <param name="item"> Объект, возвращаемый в пул. </param>
-    public void OnReleaseAmmoToPool(GameObject item)
-    {
-        //// Отключаем влияние физики, расчет применяемых сил, а затем отключаем объект.
-        //item.GetComponent<Rigidbody>().detectCollisions = false;
-        //item.GetComponent<Rigidbody>().useGravity = false;
-
-        item.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-
-        item.SetActive(false);
-    }
-
-    /// <summary>
-    /// Метод, вызываемый ObjectPool<>.Destroy().
-    /// </summary>
-    /// <param name="item"> Объект, удаляемый из пула. </param>
-    public void OnDestroyAmmoFromPool(GameObject item) => Destroy(item.gameObject);
+    // Метод, вызываемый ObjectPool<>.Destroy().
+    public void OnDestroyAmmoFromPool(Ammunition item) => Destroy(item.gameObject);
     #endregion
 }
